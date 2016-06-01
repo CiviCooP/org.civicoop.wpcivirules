b6e985f6cd1c622483887dbe037acbd74578ef08
Kevin Levie <kevin@levity.nl>
2016-06-01 21:23:30 +0200

    Add activity on user account creation + improved logging

--- CRM/WPCivi/CiviRulesActions/WPCreateUser.php
+++ CRM/WPCivi/CiviRulesActions/WPCreateUser.php
@@ -1,5 +1,7 @@
 <?php
 
+use Psr\Log\LogLevel;
+
 /**
  * Class CRM_WPCivi_CiviRulesActions_WPCreateUser (...namespaces are overrated)
  * CiviRules action to create a WordPress user account for a contact, if it doesn't exist yet.
@@ -8,17 +10,29 @@ class CRM_WPCivi_CiviRulesActions_WPCreateUser extends \CRM_Civirules_Action
 {
 
     /**
+     * Possible action status results. Used for logging and activity creation (see the logWPAction method).
+     */
+    const ACTION_COMPLETED = 'Completed';
+    const ACTION_CANCELLED = 'Cancelled';
+    const ACTION_NOT_REQUIRED = 'Not Required';
+
+    /**
+     * @var \CRM_Civirules_TriggerData_TriggerData $triggerData Trigger data (made available to all methods)
+     */
+    private $triggerData;
+
+    /**
      * Method to execute the action when it is triggered
      * @param \CRM_Civirules_TriggerData_TriggerData $triggerData
      * @return bool Success
      */
     public function processAction(\CRM_Civirules_TriggerData_TriggerData $triggerData)
     {
-        $logger = \CRM_WPCivi_Config::getLogger(); // Hmm, we could have used $this->logAction... oh well
+        $this->triggerData = $triggerData;
         $ruleId = $this->ruleAction['rule_id'];
 
         if(!\CRM_WPCivi_Config::isWordPress()) {
-            $logger->error('WPCreateUser: could not execute action, not running on WordPress!');
+            $this->logWPAction('WPCreateUser: could not execute action, not running on WordPress!', 'error');
             return false;
         }
 
@@ -26,39 +40,38 @@ class CRM_WPCivi_CiviRulesActions_WPCreateUser extends \CRM_Civirules_Action
         $actionParams = $this->getActionParameters(); // contains [wp_role]
         // $groupData = $triggerData->getEntityData('GroupContact'); // [group_id|contact_id|status] - not needed here
 
+        // ---------------------
         // Check for existing WP user
         try {
             $ufmatch = civicrm_api3('UFMatch', 'getsingle', ['contact_id' => $contactId]);
             if($ufmatch && isset($ufmatch['uf_id'])) {
-                $logger->info('WPCreateUser: no user created, contact already linked with WP user ' . $ufmatch['uf_id'] . ' (' . $ufmatch['uf_name'] . ')', ['contact_id' => $contactId, 'rule_id' => $ruleId]);
+                $this->logWPAction('WPCreateUser: no user created, contact already linked with WP user ' . $ufmatch['uf_id'] . ' (' . $ufmatch['uf_name'] . ')', LogLevel::INFO, static::ACTION_NOT_REQUIRED);
                 return true;
             }
         } catch(\CiviCRM_API3_Exception $e) {
             // No match, continuing
         }
 
-        // Create new WP user - TODO discuss what's the best approach here (passwords, welcome mail, etc)
+        // ---------------------
+        // Create new WP user
 
         // Fetch contact data
         try {
             $contact = civicrm_api3('Contact', 'getsingle', ['id' => $contactId]);
 
             if(!$contact || empty($contact['email'])) {
-                $logger->warning('WPCreateUser: contact has no primary email address, action cancelled.', ['contact_id' => $contactId, 'rule_id' => $ruleId]);
-                return false;
+                return $this->logWPAction('WPCreateUser: contact has no primary email address, action cancelled.', LogLevel::WARNING, static::ACTION_CANCELLED);
             }
         } catch(\CiviCRM_API3_Exception $e) {
-            $logger->warning('WPCreateUser: could not fetch contact record (' . $e->getMessage() . ').', ['contact_id' => $contactId, 'rule_id' => $ruleId]);
-            return false;
+            return $this->logWPAction('WPCreateUser: could not fetch contact record (' . $e->getMessage() . ').', LogLevel::WARNING, static::ACTION_CANCELLED);
         }
 
         // Check if username (email) exists
         if(username_exists($contact['email']) != false) {
-            $logger->warning('WPCreateUser: a WP account with email address ' . $contact['email'] . ' already exists, but it is not matched to contact.', ['contact_id' => $contactId, 'rule_id' => $ruleId]);
-            return false;
+            return $this->logWPAction('WPCreateUser: a WP account with email address ' . $contact['email'] . ' already exists, but it is not linked with this contact.', LogLevel::WARNING, static::ACTION_CANCELLED);
         }
 
-        // Try to create user and set name, email, role
+        // Try to create WP user and set name, email, role
         $userParams = [
             'user_login' => $contact['email'],
             'user_pass' => md5(mt_rand()),
@@ -71,8 +84,7 @@ class CRM_WPCivi_CiviRulesActions_WPCreateUser extends \CRM_Civirules_Action
         $uf_id = wp_insert_user($userParams);
 
         if(empty($uf_id) || $uf_id instanceof \WP_Error) {
-            $logger->warning('WPCreateUser: calling wp_insert_user failed for contact ' . $contactId . ' (parameters: ' . json_encode($userParams) . ').', ['contact_id' => $contactId, 'rule_id' => $ruleId]);
-            return false;
+            return $this->logWPAction('WPCreateUser: calling wp_insert_user failed for contact ' . $contactId . ' (parameters: ' . json_encode($userParams) . ').', LogLevel::ERROR, static::ACTION_CANCELLED);
         }
 
         // Create UFMatch record (double check if this is not done automatically)
@@ -83,14 +95,11 @@ class CRM_WPCivi_CiviRulesActions_WPCreateUser extends \CRM_Civirules_Action
                 'uf_name' => $contact['email'],
             ]);
         } catch(\CiviCRM_API3_Exception $e) {
-            $logger->warning('WPCreateUser: WordPress account was created, but creating UFMatch record failed for contact_id ' . $contactId . ' and uf_id ' . $uf_id . ' (' . $e->getMessage() . ').',
-                ['contact_id' => $contactId, 'rule_id' => $ruleId]);
-            return false;
+            return $this->logWPAction('WPCreateUser: WordPress account was created, but creating UFMatch record failed for contact_id ' . $contactId . ' and uf_id ' . $uf_id . ' (' . $e->getMessage() . ').', LogLevel::WARNING, static::ACTION_CANCELLED);
         }
 
         // That should be all for now
-        $logger->info('WPCreateUser: created WP user ' . $uf_id . ' (' . $contact['email'] . ') with role "' . ucfirst($actionParams['wp_role']) . '".', ['contact_id' => $contactId, 'rule_id' => $ruleId]);
-        return true;
+        return $this->logWPAction('WPCreateUser: created WP user ' . $uf_id . ' (' . $contact['email'] . ') with role "' . ucfirst($actionParams['wp_role']) . '".', LogLevel::INFO, static::ACTION_COMPLETED);
     }
 
     /**
@@ -114,5 +123,54 @@ class CRM_WPCivi_CiviRulesActions_WPCreateUser extends \CRM_Civirules_Action
             $label .= ' with role: "' . ucfirst($params['wp_role']) . '"';
         }
         return $label;
+    }
+
+    /**
+     * Log an action using the internal logAction method (database logging should be enabled),
+     * and try to create an activity based on the result of running this action.
+     * @param string $message Log message
+     * @param string $logLevel Log level (constant from \Psr\Log\LogLevel)
+     * @param string $actionResult Action result (constant from this class)
+     * @return bool Success
+     */
+    public function logWPAction($message, $logLevel = LogLevel::INFO, $actionResult = self::ACTION_CANCELLED)
+    {
+        // Log to CiviRulesLogger
+        $this->logAction($message, $this->triggerData, $logLevel);
+
+        // Create activity for contact, if possible (-> activity type should be added by extension)
+        try {
+            switch($actionResult) {
+                case self::ACTION_COMPLETED:
+                    $subject = 'Succesfully created a WordPress account';
+                    break;
+                case self::ACTION_NOT_REQUIRED:
+                    $subject = 'Contact already has a WordPress account';
+                    break;
+                case self::ACTION_CANCELLED:
+                    $subject = 'An error occurred creating a WordPress account';
+                    break;
+                default:
+                    $subject = 'Unknown action result for WPCreateUser';
+                    break;
+            }
+
+            civicrm_api3('Activity', 'create', [
+                'activity_type_id' =>  'CiviRules_WPCreateUser_Result',
+                'status_id' => $actionResult,
+                'target_id' => $this->triggerData->getContactId(),
+                'subject' => $subject,
+                'details' => $message,
+            ]);
+
+        } catch(\CiviCRM_API3_Exception $e) {
+            if($actionResult == self::ACTION_CANCELLED) {
+                $this->logAction('WPCreateUser: an error occurred while creating an activity for an error that occurred (' . $e->getMessage() . ').', $this->triggerData, LogLevel::WARNING);
+            } else {
+                $this->logAction('WPCreateUser: action has succesfully run, but an error occurred while creating an activity (' . $e->getMessage() . ').', $this->triggerData, LogLevel::WARNING);
+            }
+        }
+
+        return ($actionResult != self::ACTION_CANCELLED);
     }
 }
--- README.md
+++ README.md
@@ -4,4 +4,6 @@ org.civicoop.wpcivirules
 This extension adds CiviRules actions/triggers that interact with the underlying Wordpress installation.  
 Requires [CiviRules](https://github.com/civicoop/org.civicoop.civirules), obviously, as well as [CiviRulesLogger](https://github.com/civicoop/org.civicoop.civiruleslogger).  
 
-For now, this extension only adds an action to add user accounts (currently Wordpress-only, could probably be rewritten to support all UFs). More WordPress-related functionality *may* be added in the future.  
+For now, this extension only adds an action to add WP user accounts.   
+More WordPress-related triggers/actions *may* be added in the future.  
+This functionality is currently Wordpress-only, but could be rewritten to support all UFs. However, similar functionality in Drupal could probably be implemented with Drupal Rules + Entities just as well.  
