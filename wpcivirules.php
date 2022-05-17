9fc776abd60f1ef7be0872469a667f983ed500d0
Richard Baugh <richard@graydigitalgroup.com>
2022-05-17 09:18:13 -0500

    Ran civix upgrade to update extention to meet new framework standards and to make it work for PHP8

--- wpcivirules.php
+++ wpcivirules.php
@@ -34,17 +34,6 @@ function wpcivirules_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @param $files array(string)
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function wpcivirules_civicrm_xmlMenu(&$files) {
-  _wpcivirules_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -96,50 +85,3 @@ function wpcivirules_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
   return _wpcivirules_civix_civicrm_upgrade($op, $queue);
 }
 
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function wpcivirules_civicrm_managed(&$entities) {
-  _wpcivirules_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function wpcivirules_civicrm_caseTypes(&$caseTypes) {
-  _wpcivirules_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function wpcivirules_civicrm_angularModules(&$angularModules) {
-_wpcivirules_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function wpcivirules_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _wpcivirules_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
